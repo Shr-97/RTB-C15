b0251024afe2f119bc992496aaa535ed612963f1
Shr-97 <69622118+Shr-97@users.noreply.github.com>
2026-03-18 17:31:40 +0545

    Update main.tf

--- .github/terraform/main.tf
+++ .github/terraform/main.tf
@@ -1,1 +1,11 @@
+provider "aws" { region = "us-east-1" }
 
+resource "aws_ecr_repository" "app_repo" {
+  name = "devops-rtb-c15-app"
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "rtb-cluster"
+}
+
+# (Add ECS Service and Task Definitions here focusing on EC2 launch type)
